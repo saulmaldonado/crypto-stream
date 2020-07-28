92a753fe5c5cfd7f46885417b8bf7284eccde549
Saul Maldonado <samaldonado4@gmail.com>
2020-07-28 03:07:25 -0500

    Add script to initialize MongoDB database

---
+++ initDatabase.ts
@@ -0,0 +1,54 @@
+import { MongoClient } from 'mongodb';
+import { config } from 'dotenv';
+import { exit, stdin, stdout } from 'process';
+import { createInterface } from 'readline';
+
+config();
+
+const reset: boolean = process.argv.includes('--reset');
+
+const uri: string | undefined = process.env.CONNECTION_STRING;
+if (!uri) throw new Error('Connection string not provided.');
+
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async () => {
+  try {
+    const conn = await client.connect();
+    if (reset) {
+      await new Promise<void>((res, rej) => {
+        const r1 = createInterface({
+          input: stdin,
+          output: stdout,
+        });
+        try {
+          r1.question(
+            'Are you sure you want to reset your cryptoTracker database? [Y/n]: ',
+            async (answer) => {
+              if (answer.toLowerCase() === 'y' || answer.toLowerCase() === 'yes') {
+                await conn.db('cryptoTracker').dropDatabase();
+                console.log('cryptoTracker Database dropped');
+                r1.close();
+                res();
+              } else {
+                r1.close();
+                exit(0);
+              }
+            }
+          );
+        } catch {
+          r1.close();
+          rej();
+        }
+      });
+    }
+
+    await conn.db('cryptoTracker').createCollection('users');
+    await conn.db('cryptoTracker').createCollection('auth');
+
+    console.log('Database initialized');
+    exit(0);
+  } catch {
+    exit(1);
+  }
+})();
