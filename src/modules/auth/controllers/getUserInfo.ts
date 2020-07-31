ce3834c6769dee2bcaf9aae68624f020e936cc70
Saul Maldonado <samaldonado4@gmail.com>
2020-07-31 03:57:35 -0500

    Add method for decoding userID from access token

--- src/modules/auth/controllers/getUserInfo.ts
+++ src/modules/auth/controllers/getUserInfo.ts
@@ -1,4 +1,6 @@
+import { isJWT } from 'class-validator';
 import { decode } from 'jsonwebtoken';
+import { IsUserID, IsUserIDConstraint } from '../input/validators/IsUserID';
 
 type IdToken = {
   nickname: string;
@@ -14,6 +16,27 @@ type IdToken = {
   exp: number;
 };
 
+type AccessToken = {
+  iss: string;
+  sub: string;
+  aud: string;
+  iat: number;
+  exp: number;
+  azp: string;
+  scope: string;
+  gty: 'password';
+};
+
 export const getUserInfo = (id_token: string): IdToken => {
   return decode(id_token) as IdToken;
+};
+
+export const getUserID = (access_token: string): string => {
+  if (!isJWT(access_token)) throw new Error('Not valid JWT access token');
+
+  const { sub: userID } = decode(access_token) as AccessToken;
+
+  if (!new IsUserIDConstraint().validate(userID)) throw new Error('Not Valid User ID');
+
+  return userID;
 };
