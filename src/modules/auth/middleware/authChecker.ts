93b181c9d728d53b05667f4d7cd7690360199841
Saul Maldonado <samaldonado4@gmail.com>
2020-08-01 21:47:41 -0500

    Add check for iss,aud,azp in JWTtoken in authChecker

--- src/modules/auth/middleware/authChecker.ts
+++ src/modules/auth/middleware/authChecker.ts
@@ -1,47 +1,63 @@
 import { AuthChecker } from 'type-graphql';
+import jwksRsa from 'jwks-rsa';
+import { decode, JwtHeader, Secret, verify } from 'jsonwebtoken';
+import { ApolloError } from 'apollo-server-express';
 
 import { Context } from './Context';
-import jwksRsa from 'jwks-rsa';
-import { decode, Secret, verify } from 'jsonwebtoken';
-import { ApolloError } from 'apollo-server-express';
 
 export const customAuthChecker: AuthChecker<Context> = async ({ context }) => {
   const { req } = context;
-  if (req.headers.authorization) {
-    const token = req.headers.authorization.split(' ')[1];
-    const secret = jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-    });
+  if (!req.headers.authorization) return false;
+  const token = req.headers.authorization.split(' ')[1];
+  const secret = jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  });
 
-    const decoded = decode(token, { complete: true });
-    if (!decoded) throw new ApolloError('Unable to verify JWT', 'INTERNAL_SERVER_ERROR');
+  const decoded = decode(token, { complete: true });
+  if (!decoded) return false;
 
-    const { header, payload } = decoded as {
-      header: Record<string, string>;
-      payload: Record<string, string>;
-      signature: string;
-    };
+  const { header, payload } = decoded as {
+    header: Record<string, string>;
+    payload: Record<string, string>;
+    signature: string;
+  };
 
-    try {
-      const decodedToken = await new Promise<string | object | never>((res, rej) =>
-        secret(req, header, payload, (err, secret) => {
-          if (err || !secret)
-            rej(new ApolloError(err ?? 'Unable to verify JWT', 'INTERNAL_SERVER_ERROR'));
-          const decodedToken = verify(token, secret as Secret);
-          res(decodedToken);
-        })
-      );
+  try {
+    const decodedToken = await new Promise<JWTPayload | never>((res, rej) =>
+      secret(req, header, payload, (err, secret) => {
+        if (err || !secret)
+          rej(new ApolloError(err ?? 'Unable to verify JWT', 'INTERNAL_SERVER_ERROR'));
+        const decodedToken = verify(token, secret as Secret) as JWTPayload;
+        res(decodedToken);
+      })
+    );
 
-      console.log(decodedToken);
+    const { aud, iss, azp } = decodedToken;
 
-      return true;
-    } catch (error) {
+    if (
+      !(iss === `https://${process.env.AUTH0_DOMAIN}/`) ||
+      !(aud![0] === process.env.AUTH0_AUDIENCE) ||
+      !(azp === process.env.AUTH0_CLIENT_ID)
+    ) {
       return false;
     }
-  } else {
-    return false; // or false if access is denied
+
+    return true;
+  } catch (error) {
+    return false;
   }
+};
+
+type JWTPayload = {
+  iss?: string;
+  sub?: string;
+  aud?: string[];
+  iat?: number;
+  exp?: number;
+  azp?: string;
+  scope?: string;
+  gty?: string;
 };
