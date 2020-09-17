cf440855506a1211d59b067639ff6209e6ff5925
Saul Maldonado <samaldonado4@gmail.com>
2020-09-17 11:44:37 -0500

    Implement refresh APIKey resolver

--- src/modules/auth/APIKey.ts
+++ src/modules/auth/APIKey.ts
@@ -1,11 +1,17 @@
 import { Ctx, Query, Resolver } from 'type-graphql';
 import { Context } from './middleware/Context';
 import { getKey } from './controllers/getAPIKey';
+import { refreshAPIKey } from './controllers/refreshAPIKey';
 
 @Resolver()
 export class APIKeyResolver {
   @Query(() => String)
   async getAPIKey(@Ctx() ctx: Context): Promise<string | never> {
     return await getKey(ctx);
+  }
+
+  @Query(() => String)
+  async refreshAPIKey(@Ctx() ctx: Context) {
+    return await refreshAPIKey(ctx);
   }
 }
