bf3edcce0e7ed35435a6d3fffc2cc1e4854c6eed
Saul Maldonado <samaldonado4@gmail.com>
2020-09-18 05:58:03 -0500

    remove hello query

--- src/modules/auth/verifyEmail.ts
+++ src/modules/auth/verifyEmail.ts
@@ -1,15 +1,10 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Resolver } from 'type-graphql';
 
 import { verifyEmail } from './controllers/verifyEmailController';
 import { VerifyEmailInput } from './input/verifyEmailInput';
 
 @Resolver()
 export class EmailResolver {
-  @Query(() => String)
-  hello() {
-    return 'hello';
-  }
-
   @Mutation(() => Boolean)
   async sendEmailVerification(@Arg('data') { userID }: VerifyEmailInput): Promise<true | never> {
     await verifyEmail(userID);
