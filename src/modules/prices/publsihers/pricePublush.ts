68b0872ae336738707442641c1b884293d2f6399
Saul Maldonado <samaldonado4@gmail.com>
2020-09-18 23:08:33 -0500

    Implement caching for fetchAndPublish

--- src/modules/prices/publsihers/pricePublush.ts
+++ src/modules/prices/publsihers/pricePublush.ts
@@ -2,13 +2,23 @@ import { RedisPubSub } from 'graphql-redis-subscriptions';
 
 import { pricePublishedInit } from './helpers/pricePublisherInit';
 import { fetchPrices } from '../controllers/helpers/fetchCoinPrices';
-import { allCoinIDs } from '../../../config/coinIDsConfig';
+import { redis } from '../../..';
+import { CoinRanking, PricePayload } from '../prices';
 
 const fetchAndPublish = async (pubSub: RedisPubSub) => {
-  // fetches the top 100 coins from coinIDsConfig file
-  const coinIDs: string[] = allCoinIDs.slice(0, 100);
+  // fetch the coin rankings from cache (rankings have 10 expiry)
+  const res = await redis.get('rankings');
+  let coins: PricePayload[] = [];
 
-  const coins = await fetchPrices({ coinIDs });
+  if (!res) {
+    // if cache is empty, fetch all prices and filter to the top 100. This will also set ranking cache again
+    coins = await fetchPrices({ limit: 100 });
+  } else {
+    // map the cache to coinIDs array and fetch the coins
+    let rankings: CoinRanking[] = JSON.parse(res);
+    const coinIDs: string[] = rankings.slice(0, 100).map((coin) => coin.name);
+    coins = await fetchPrices({ coinIDs });
+  }
 
   pubSub.publish('PRICES', coins);
 };
