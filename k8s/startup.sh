e9c50d84e159e0ae2690c4d15c3df0fa5a4ffdcf
Saul Maldonado <samaldonado4@gmail.com>
2020-10-04 22:37:06 -0500

    feat: add k8s files and script for deploying to cluster

---
+++ k8s/startup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# start nginx-ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/do/deploy.yaml
+kubectl apply -f ingress.yaml
+
+# start mongodb replicaset
+kubectl apply -f mongo-price-api.yaml
+
+# start redis cluster
+helm install crypto-redis --set master.persistence.size=1Gi,slave.persistence.size=1Gi bitnami/redis
+
+export REDIS_PASSWORD=$(kubectl get secret --namespace default crypto-redis-redis-cluster -o jsonpath="{.data.redis-password}" | base64 --decode)
+export CLUSTER_REDIS_HOST=crypto-redis-master.default.svc.cluster.local
+
+# copy and run coinIds redis script
+kubectl cp coinIds.redis crypto-redis-master-0:/tmp/
+kubectl exec -it crypto-redis-master-0 -- /bin/bash
+cd tmp
+redis-cli -a $REDIS_PASSWORD < coinIds.redis
+exit
+
+# add redis password to .env
+echo REDIS_PASSWORD=$REDIS_PASSWORD\n >> .env
+
+# generate server env for server
+kubectl create secret generic price-api-env --from-env-file=.env
+
+# start server 
+kubectl apply -f graphQLServerDeployment.yaml
+
+# display server logs
+export PRICE_API_POD=$(kubectl get pod -l app=price-api -o jsonpath="{.items[0].metadata.name}")
+kubectl logs PRICE_API_POD -f
