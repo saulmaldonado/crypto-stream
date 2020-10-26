2b129457aea26b50aa7a3a941d4f58919854f8dc
Saul Maldonado <samaldonado4@gmail.com>
2020-10-26 18:28:27 -0500

    test: refactor tests and add failing db test

--- src/__test__/getAPIKey.test.ts
+++ src/__test__/getAPIKey.test.ts
@@ -2,7 +2,8 @@ import { mongoose } from '@typegoose/typegoose';
 import { config } from 'dotenv';
 import { KeyModel } from '../models/Key';
 import { Context } from '../modules/auth/middleware/Context';
-import * as tokenMethods from '../modules/auth/jwt/getTokenUserID';
+import { getTokenUserID } from '../modules/auth/jwt/getTokenUserID';
+// import * as getTokenUserIDModule from '../modules/auth/jwt/getTokenUserID';
 import { getKey } from '../modules/apiKey/controllers/getAPIKey';
 import { generateAPIKey } from '../modules/apiKey/controllers/helpers/keyFunctions';
 import { redis } from '../utils/redisCache';
@@ -10,29 +11,29 @@ import { redis } from '../utils/redisCache';
 config();
 
 const userID: string = 'auth0|5f6aa02c4419aa00717f9ee8';
-
-beforeAll(async () => {
-  await mongoose.connect(`${process.env.MONGO_URI}/test5`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
-});
-
-afterAll(async () => {
-  await mongoose.disconnect();
-});
+let getTokenUserIDMock: jest.SpyInstance<
+  ReturnType<typeof getTokenUserID>,
+  Parameters<typeof getTokenUserID>
+>;
 
 describe('getAPIKey: controller', () => {
-  let getTokenUserIDMock: jest.SpyInstance<string, [Context]>;
+  beforeAll(async () => {
+    await mongoose.connect(`${process.env.MONGO_URI}/test5`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
 
-  beforeAll(() => {
-    getTokenUserIDMock = jest.spyOn(tokenMethods, 'getTokenUserID');
+    getTokenUserIDMock = jest.spyOn(
+      await import('../modules/auth/jwt/getTokenUserID'),
+      'getTokenUserID'
+    );
     getTokenUserIDMock.mockImplementation(() => userID);
   });
 
-  afterAll(() => {
+  afterAll(async () => {
+    await mongoose.disconnect();
     getTokenUserIDMock.mockRestore();
     redis.disconnect();
   });
@@ -70,6 +71,29 @@ describe('getAPIKey: controller', () => {
         key,
         timestamp,
       });
+    });
+  });
+
+  describe('getAPIKey: database error', () => {
+    let getKeyMock: jest.SpyInstance<
+      ReturnType<typeof KeyModel.findOne>,
+      Parameters<typeof KeyModel.findOne>
+    >;
+    beforeAll(async () => {
+      getKeyMock = jest.spyOn((await import('../models/Key')).KeyModel, 'findOne');
+      getKeyMock.mockImplementation(() => {
+        throw new Error(); // mock database failing
+      });
+    });
+
+    afterAll(() => {
+      getKeyMock.mockRestore();
+    });
+
+    it('should throw server error when database fails', async () => {
+      await expect(async () => {
+        await getKey({} as Context);
+      }).rejects.toThrow();
     });
   });
 });
