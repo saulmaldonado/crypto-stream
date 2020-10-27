51a7a6fb77c6e08110bddd8b9371f4620f1da026
Saul Maldonado <samaldonado4@gmail.com>
2020-10-27 00:59:15 -0500

    tests: add edge case test for non-matching auth0 credentials

--- src/__test__/authChecker.test.ts
+++ src/__test__/authChecker.test.ts
@@ -1,17 +1,26 @@
 /* eslint-disable jest/valid-expect */
 import { ResolverData } from 'type-graphql';
 import { config } from 'dotenv';
-import { getInvalidTestingToken } from './utils/getTestingToken';
+import { getInvalidTestingToken, getTestingToken } from './utils/getTestingToken';
 import { customAuthChecker } from '../modules/auth/middleware/authChecker';
 import { Context } from '../modules/auth/middleware/Context';
 
 config();
 
 let invalidToken: string;
+let validToken: string;
+const { AUTH0_CLIENT_ID } = process.env;
+const { AUTH0_API_ID } = process.env;
+
 beforeAll(async () => {
   invalidToken = getInvalidTestingToken();
+  validToken = await getTestingToken();
 });
 
+afterAll(() => {
+  process.env.AUTH0_CLIENT_ID = AUTH0_CLIENT_ID;
+  process.env.AUTH0_API_ID = AUTH0_API_ID;
+});
 describe('authChecker', () => {
   it('should unauthorize request with missing token', async () => {
     const result = await customAuthChecker({ context: {} } as ResolverData<Context>, []);
@@ -25,5 +34,16 @@ describe('authChecker', () => {
     expect(async () => {
       await customAuthChecker(context as ResolverData<Context>, []);
     }).rejects.toThrow();
+  });
+
+  it('should unauthorize for non-matching auth0 credentials', async () => {
+    process.env.AUTH0_CLIENT_ID = 'invalidID';
+    process.env.AUTH0_API_ID = 'invalidID';
+
+    const context = { context: { token: validToken } };
+
+    const result = await customAuthChecker(context as ResolverData<Context>, []);
+
+    expect(result).toBe(false);
   });
 });
